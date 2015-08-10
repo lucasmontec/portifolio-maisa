f833c87facb160c79eaa380ff0f238a021eafd7f
Lucas M Carvalhaes <lucas_carvalhaes@hotmail.com>
2015-08-10 18:14:34 -0300

    improved job listing and added features to flow. ajax now.

--- maisa-bolt/theme/maisa-template/flow.js
+++ maisa-bolt/theme/maisa-template/flow.js
@@ -3,8 +3,21 @@ $(document).ready(function() {
     var T = 0;
     setInterval(function(){ 
         T++;
-        $('#back-gradient').css("opacity", 0.5+((Math.sin(T/40)+1)/4) );
-        /*console.log(0.5+((Math.sin(T/40)+1)/4) );*/
+        
+        if(sessionStorage.currentPlate == 'trabalho'){
+            var current = $('#back-gradient').css("opacity");
+            //console.log(current);
+            if(current > 0){
+                $('#back-gradient').css("opacity", current-0.1);
+            }
+        }else{
+            $('#back-gradient').css("opacity", 0.5+((Math.sin(T/40)+1)/4) );
+        }
+        
+        //Prevent
+        if(T> 1000000){
+            T=0;
+        }
     }, 50);
     
 });
